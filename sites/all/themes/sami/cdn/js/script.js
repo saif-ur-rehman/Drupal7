f7f17472c422eb9d13df4df5875b2edcad75bcae
Saif-ur-Rehman <saif54624@yahoo.com>
2016-08-13 00:16:28 +0500

    Adding CCK and references modules with changing theme Java Script files

--- sites/all/themes/sami/cdn/js/script.js
+++ sites/all/themes/sami/cdn/js/script.js
@@ -5,16 +5,48 @@ jQuery( document ).ready(function($) {
   $(this).parent().children('.links').slideToggle();
 });
 
-  $('.field-content p').hover(
-    function(){
-      $(this).addClass('hilight');
-    },
-    function(){
-      $(this).removeClass('hilight')
-    }
-    );
+  $('.field-content p').hover(function(){
+      $(this).toggleClass('hilight');
+    });
+// read more Ajax
+      // $('.node-readmore a').click(function(){
+      //   var url = $(this).attr('href');
+      //   var link = this;
+      //   var divClone = $('div.field-item').clone();
+
+      //   $.ajax({
+      //     url : url,
+      //     success: function(data){
+      //     var $fullContent = $('.field-type-text-with-summary .field-item' , data);
+      //     var html = $fullContent.html();
+      //     console.log(html);
+      //     $(link).closest('article').find('div.field-item').html(html);
+      //     $(link).text('Read less');
+      //       if (link.text === 'Read less' && link.clicked()){
+      //         $(link).closest('article').find('div.field-item').replaceWith(divClone.clone());
+      //       }
+      //     // $(link).replaceWith(divClone.clone());
+
+      //   //   $(link).text(function(i, text){
+      //   //     return text === "Read more" ? "Read less" : "Read less";
+      //   // })
+      //     // $(link).html($(link).text() == 'Read more' ? 'Read less' : 'Read more');
+      //   }
+      //   });
+      //   return false;
+      // });
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
-
 })(jQuery);
